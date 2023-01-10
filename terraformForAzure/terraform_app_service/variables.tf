0485e71dfdfbd843c5d0f899019b3a7ed669840d
Abhishek Dhingra <54358904+theabhishekdhingra@users.noreply.github.com>
2023-01-10 22:05:19 +0530

    Delete terraformForAzure directory

--- terraformForAzure/terraform_app_service/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "webapp_app_settings" { 
-    type = map 
-    default = {
-      WEBSITE_LOAD_CERTIFICATE = "*"
-      WEBSITE_TIME_ZONE = "Indian Standard Time"
-      WEBSITE_VNET_ROUTE_ALL = 1
-    }
-
-}
--- terraformForAzure/terraform_chapter2/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "client_secret" {
-  type = string
-  sensitive = true
-}
-
-#  nX78Q~4p4Yrs-ESlUXpErwAoRRUvveBL45UvHb3q
